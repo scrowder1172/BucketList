bbc874e5bd98259515e6446691c49d9da4ef3194
Scott Crowder <scrowder@gmail.com>
2024-01-08 14:11:20 -0600

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by SCOTT CROWDER on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
