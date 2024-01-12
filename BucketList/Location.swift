871135d770537b53c92deb7d8827c1b4f5016814
Scott Crowder <scrowder@gmail.com>
2024-01-12 10:48:17 -0600

    add example location and custom equatible function

--- BucketList/Location.swift
+++ BucketList/Location.swift
@@ -18,4 +18,12 @@ struct Location: Codable, Equatable, Identifiable {
     var coordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
+    
+    #if DEBUG
+    static let example: Location = Location(id: UUID(), name: "Buckingham Palace", description: "Lit by over 40,000 lightbulbs.", latitude: 51.501, longitude: -0.141)
+    #endif
+    
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
