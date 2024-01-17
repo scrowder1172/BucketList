e28b9eb6342f2cc3f548955080bc59123a4de6c6
Scott Crowder <scrowder@gmail.com>
2024-01-17 12:56:18 -0600

    switch between map views

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -19,62 +19,77 @@ struct ContentView: View {
         )
     )
     
+    @State private var standardMapMode: Bool = false
+    @State private var isPresented: Bool = false
+    
     var body: some View {
         if viewModel.isUnlocked {
-            ZStack(alignment: .topTrailing) {
-                ZStack(alignment: .centerLastTextBaseline) {
-                    MapReader { proxy in
-                        Map(initialPosition: startPosition) {
-                            // display saved locations
-                            ForEach(viewModel.locations) { location in
-                                Annotation(location.name,
-                                           coordinate: location.coordinate
-                                ) {
-                                    Image(systemName: "star.circle")
-                                        .resizable()
-                                        .foregroundStyle(.red)
-                                        .frame(width: 44, height: 44)
-                                        .background(.white)
-                                        .clipShape(.circle)
-                                        .onLongPressGesture {
-                                            viewModel.selectedPlace = location
-                                        }
+            ZStack(alignment: .topLeading) {
+                ZStack(alignment: .topTrailing) {
+                    ZStack(alignment: .centerLastTextBaseline) {
+                        MapReader { proxy in
+                            Map(initialPosition: startPosition) {
+                                // display saved locations
+                                ForEach(viewModel.locations) { location in
+                                    Annotation(location.name,
+                                               coordinate: location.coordinate
+                                    ) {
+                                        Image(systemName: "star.circle")
+                                            .resizable()
+                                            .foregroundStyle(.red)
+                                            .frame(width: 44, height: 44)
+                                            .background(.white)
+                                            .clipShape(.circle)
+                                            .onLongPressGesture {
+                                                viewModel.selectedPlace = location
+                                            }
+                                    }
+                                }
+                            }
+                            .mapStyle(standardMapMode ? .standard : .hybrid)
+                            .onMapCameraChange(frequency: .continuous) { context in
+                                viewModel.setCurrentLocation(coordinates: context.region.center)
+                            }
+                            .onTapGesture { position in
+                                if let coordinate = proxy.convert(position, from: .local) {
+                                    viewModel.addLocation(at: coordinate)
+                                }
+                            }
+                            .sheet(item: $viewModel.selectedPlace) { place in
+                                //Text(place.name)
+                                EditView(location: place) {
+                                    viewModel.updateLocation(location: $0)
                                 }
                             }
                         }
-                        .mapStyle(.hybrid)
-                        .onMapCameraChange(frequency: .continuous) { context in
-                            viewModel.setCurrentLocation(coordinates: context.region.center)
-                        }
-                        .onTapGesture { position in
-                            if let coordinate = proxy.convert(position, from: .local) {
-                                viewModel.addLocation(at: coordinate)
-                            }
-                        }
-                        .sheet(item: $viewModel.selectedPlace) { place in
-                            //Text(place.name)
-                            EditView(location: place) {
-                                viewModel.updateLocation(location: $0)
-                            }
-                        }
+                        
+                        Text("Lat: \(viewModel.currentLat.formatted()), Long: \(viewModel.currentLong.formatted())")
+                            .padding(10)
+                            .font(.caption)
+                            .background(.green)
+                            .foregroundStyle(.black)
+                            .clipShape(.capsule)
+                            .offset(x: -20, y: -20)
                     }
-                    
-                    Text("Lat: \(viewModel.currentLat.formatted()), Long: \(viewModel.currentLong.formatted())")
+                    Text("Lat:\(viewModel.tapLat.formatted()), Long:\(viewModel.tapLong.formatted())")
                         .padding(10)
                         .font(.caption)
-                        .background(.green)
+                        .background(.blue)
                         .foregroundStyle(.black)
                         .clipShape(.capsule)
-                        .offset(x: -20, y: -20)
+                        .offset(x: -20, y: 0)
                 }
-                Text("Lat:\(viewModel.tapLat.formatted()), Long:\(viewModel.tapLong.formatted())")
-                    .padding(10)
-                    .font(.caption)
-                    .background(.blue)
-                    .foregroundStyle(.black)
-                    .clipShape(.capsule)
-                    .offset(x: -20, y: 0)
+                Button {
+                    standardMapMode.toggle()
+                } label: {
+                    Image(systemName: "gear")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .background(.black)
+                }
+                .offset(x: 20, y: 20)
             }
+            
         } else {
             Button("Unlock data") {
                 viewModel.authenticate()
